b5245d572d6c5db1d7a9217db3dd7a7395820d55
eben2840 <ebenmills200@gmail.com>
2022-08-29 20:02:26 +0000

    inittial

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def login():
         user = [x for x in users if x.username == username][0]
         if user and user.password == password:
             session['user_id'] = user.id
-            return redirect(url_for('index'))
+            return redirect(url_for('test'))
         return redirect(url_for('login'))
     return render_template('login.html')
 
