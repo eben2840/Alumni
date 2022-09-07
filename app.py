080e6fefd250d356a9ac30f4fd07662380c87aa3
eben2840 <ebenmills200@gmail.com>
2022-09-07 11:29:41 +0000

    new pushh

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from telnetlib import LOGOUT
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, redirect, render_template, url_for,request,jsonify,get_flashed_messages
-from forms import *
 from flask_migrate import Migrate
 import json
 from flask_marshmallow import Marshmallow
@@ -9,9 +8,6 @@ from flask import(
 Flask,g,redirect,render_template,request,session,url_for,flash,jsonify
 )
 from flask_cors import CORS
-
-
-
 
 app=Flask(__name__)
 CORS(app)
@@ -23,12 +19,17 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
 
+from forms import *
+
 ''''''
 
 
 
 #login for admin
 class User:
+    username = StringField('username', validators=[DataRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=80)])
+
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
@@ -52,71 +53,28 @@ def before_request():
         user = [x for x in users if x.id == session['user_id']][0]
         g.user = user
 
+
 #DATABASE MODEL
 #person table
 class Person(db.Model):
     id= db.Column(db.Integer, primary_key=True)
-    lastname= db.Column(db.String(200), nullable=True, unique=True)
-    Gender= db.Column(db.String(10), nullable=True)
-    Othername = db.Column(db.Integer(),nullable = True)  
-    Primary = db.Column(db.Integer(),nullable = True) 
-    
-    Other = db.Column(db.Integer(),nullable = True)  
-    Kin = db.Column(db.Integer(),nullable = True)  
-    Relationship = db.Column(db.Integer(),nullable = True)  
-    Home = db.Column(db.Integer(),nullable = True)  
-    Current = db.Column(db.Integer(),nullable = True)  
-    Nationality = db.Column(db.Integer(),nullable = True)  
-  
+    name= db.Column(db.String(200), nullable=True, unique=True)
+    yearCompleted= db.Column(db.String(200), nullable=True, unique=True)
+    nationality= db.Column(db.String(200), nullable=True, unique=True)
+    contact= db.Column(db.String(200), nullable=True, unique=True)
+    email= db.Column(db.String(200), nullable=True, unique=True)
+    faculty= db.Column(db.String(200), nullable=True, unique=True)
+    hallofresidence= db.Column(db.String(200), nullable=True, unique=True)
     
     def __repr__(self):
-        return f"Person('{self.id}', {self.lastname}', {self.Othername})"
+        return f"Person('{self.id}', {self.name}', {self.yearCompleted})"
 
 
 
-#yeargroup table
-class YearGroup(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String(200), nullable=True)
-    program= db.Column(db.String(200), nullable=True)
-    total_number= db.Column(db.Integer(), nullable=True)
-    people_completed= db.Column(db.Integer(),nullable = True)
-    people_admitted= db.Column(db.Integer(),nullable = True)
-
-    def __repr__(self):
-        return f"Course('{self.id}', {self.name}',)"
     
-    
-    
-    
-#department table
-class School(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String(200), nullable=True)
-    name_of_school= db.Column(db.String(200), nullable=True)
-    programs= db.Column(db.String(), nullable=True)
-    total_number = db.Column(db.Integer(),nullable = True)
-    
-    def __repr__(self):
-        return f"Course('{self.id}', {self.name}',)"
-    
-    
-    
-    
-       
-#programs table
-class Program(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String(200), nullable=True)
-    program_name= db.Column(db.String(200), nullable=True)
-    program_department= db.Column(db.String(), nullable=True)
-    program_code= db.Column(db.Integer(),nullable = True)
-    
-    def __repr__(self):
-        return f"Course('{self.id}', {self.name}',)"
 
 
-#postman  
+
     class ProductSchema(ma.Schema):
         class Meta:
             fields = ("id","name", "age", "gender")
@@ -125,8 +83,6 @@ class Program(db.Model):
 
 
 #routes 
-#GET and POST method is working
-
 
 @app.route('/test')
 def test():
@@ -153,7 +109,13 @@ def newforms():
         print(form.lastname.data)
     
     return render_template("newforms.html", form=form)
-    
+   
+
+@app.route('/home',methods=['GET','POST'])
+def home():
+    persons=Person.query.all()  
+    print(persons)
+    return render_template('home.html',persons=persons)
 
 @app.route('/members')
 def members():
@@ -168,28 +130,17 @@ def schools():
 def form():
     form=RegistrationForm()
     if form.validate_on_submit():
-        print(form.lastname.data)
-        print(form.Othername.data)
-        print(form.Gender.data)
-        print (form.Primary.data)
-        
-        print (form.Other.data)
-        print (form.Kin.data)
-        print (form.Relationship.data)
-        print (form.Home.data)
-        print (form.Current.data)
-        print (form.Nationality.data)
-        
-        print (form.Marital.data)
-        print (form.Health .data)
-        print (form.Extra.data)
-
-        new=Person(lastname=form.lastname.data, Othername=form.Othername.data, Gender=form.Gender.data, Primary=form.Primary.data,
-                   Other=form.Other.data, Kin=form.Kin.data, Relationship=form.Relationship.data, Home=form.Home.data, Current=form.Current.data, Nationality=form.Nationality.data)
+       
+        new=Person(name=form.name.data, yearCompleted=form.yearCompleted.data,
+                   nationality=form.nationality.data, 
+                   contact=form.contact.data, email=form.email.data,faculty=form.faculty.data,
+                   hallofresidence=form.hallofresidence.data)
         db.session.add(new)
         db.session.commit()
         return redirect('information')
-    flash("Please a new user", "success")
+        
+    flash("please fill this form", "success")
+    print(form.errors)
     return render_template("form.html", form=form)
 
 @app.route('/information')
@@ -197,50 +148,54 @@ def information():
     persons=Person.query.order_by(Person.id.desc()).all()
     print(persons)
     return render_template("information.html", persons=persons)
-   # print(product_schema)
+ 
  
 
 
-#upgrade method 
+#CRUD(update and delete routes)
 @app.route("/update/<int:id>")
 def update(id):
+    form=RegistrationForm()
     user=Person.query.get_or_404(id)
+    if request.method== 'GET':
+        form.name.data = user.name
+        form.yearCompleted.data = user.yearCompleted
+        form.nationality.data = user.nationality
+        form.contact.data = user.contact
+        form.email.data = user.email
+        form.faculty.data = user.faculty
+        form.hallofresidence.data = user.hallofresidence    
     if request.method== 'POST':
-        print(user.lastname)
-        user.lastname=form.lastname.data
+        new=Person(id=form.id.data, name=form.name.data, 
+                   yearCompleted=form.yearCompleted.data, 
+                   nationality=form.nationality.data,
+                   contact=form.contact.data,email=form.email.data,faculty=form.faculty.data,
+                   hallofresidence=form.hallofresidence.data)
         try:    
+            db.session.add(new)
             db.session.commit()
-            return redirect('form') 
+            return redirect(url_for('information')) 
         except:
             return"errrrror"
-    else:
-        return render_template('home.html', user=user)
+    return render_template("form.html", form=form)
     
     
-    
-#delete method
+#delete route
 @app.route("/delete/<int:id>")
 def delete(id):
     delete=Person.query.get_or_404(id)
     try:
             db.session.delete(delete)
             db.session.commit()
-            return redirect('form') 
+            return redirect(url_for('information')) 
     except: 
         return "errrrrorrr"
     
-
-@app.route('/home',methods=['GET','POST'])
-def home():
-    persons=Person.query.all()  
-    print(persons)
-    return render_template('home.html',persons=persons)
 
 
 
 
 #login routes for admin
-#login
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -248,17 +203,17 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for('test'))
-        
-        else:
+        if user:
+            if user.password == password:
+                return redirect(url_for('test'))
+            else:
                 flash("wrong password-try again!")
+        else:
+            flash("wrong password-try again!")
         return redirect(url_for('login'))
     return render_template('login.html')
-
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(host='0.0.0.0', port=50000,debug=True)
+    app.run(host='0.0.0.0', port=4000,debug=True)
     
     
--- forms.py
+++ forms.py
@@ -1,31 +1,26 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, SubmitField, SelectField
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, SubmitField, SelectField, IntegerField
 from flask_wtf import FlaskForm
-from app import Person
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+# from app import Person
 
 
 class RegistrationForm(FlaskForm):
-    lastname = StringField('Lastname')
-    Othername= StringField('Othername')
-    Gender = StringField('Gender')
-    Primary = StringField('Primary')
+    id = IntegerField('id', validators=[(DataRequired())])
+    name = StringField('name', validators=[(DataRequired() )])
+    yearCompleted= SelectField('yearCompleted', choices=[(2021,2021)])
+    nationality = StringField('nationality',validators=[DataRequired()] )
+    contact= StringField('contact',validators=[ DataRequired(), Length(min=10, max=15, message="Your number shouldn't be less than 10")])
     
-    Other = StringField('Other')
-    Kin = StringField('Kin')
-    Relationship = StringField('Relationship')
-    Home = StringField('Home')
-    Current  = StringField('Current ')
-    Nationality = StringField('Nationality')
+    email = StringField('email',validators=[(DataRequired() )])
+    faculty =SelectField('faculty', choices=[(2021,2021)])
+    hallofresidence = SelectField('hallofresidence', choices=[(2021,2021)])
+    
+    
+    
+    # el4 = SelectField('el4', default='None', choices=[(user.lastname, user.lastname) for user in Person.query.all()])
   
     
-    Marital = StringField('Marital')
-    Health = StringField('Health')
-    Extra  = StringField('Extra ')
-    
-    
-    el4 = SelectField('el4', default='None', choices=[(user.lastname, user.lastname) for user in Person.query.all()])
-  
-    
-    Program = SelectField('programs', choices=[("one", "one"),("two", "two"),("three", "three")])
+    #Program = SelectField('programs', choices=[("one", "one"),("two", "two"),("three", "three")])
     submit =SubmitField('submit')
     
     
