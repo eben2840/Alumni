90603f695e28c2e44c8edb2c75aad013acbe94cb
Testbed User <testbuser@testbed.central.edu.gh>
2022-09-27 11:27:47 +0000

    removed logs

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-function toggleDropDown(){
-    document.getElementById("side-dropdown-lists").classList.toggle('side-dropdown2');
-}
-
-function toggleNavDown(){
-    document.getElementById("nav-dropdown").classList.toggle('showNavToggle');}
-    
